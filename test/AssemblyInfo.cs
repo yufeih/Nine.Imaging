a066da09295f324d2cc5370ae2e136a130f0c209
Yufei Huang <yufeih@live.com>
2015-03-22 21:18:37 +0800

    Initial

---
+++ test/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Resources;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Nine.Imaging")]
+[assembly: AssemblyDescription("A portable image library")]
+[assembly: AssemblyCompany("Studio Nine")]
+[assembly: AssemblyProduct("Nine.Imaging")]
+[assembly: AssemblyCopyright("Copyright © Studio Nine 2015")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
