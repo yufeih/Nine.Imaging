ab12bfb382fc6d8cdf2b266bec9346d4ea8c1f17
Yufei Huang <yufeih@live.com>
2015-03-22 21:49:47 +0800

    Initial work in progress

---
+++ src/Types.cs
@@ -0,0 +1,25 @@
+﻿// ===============================================================================
+// Color.cs
+// .NET Image Tools
+// ===============================================================================
+// Copyright (c) .NET Image Tools Development Group. 
+// All rights reserved.
+// ===============================================================================
+
+namespace ImageTools
+{
+    public struct Color
+    {
+        public byte R, G, B, A;
+    }
+
+    public struct Rect
+    {
+        public double X, Y, Width, Height;
+    }
+
+    public struct Point
+    {
+        public int X, Y;
+    }
+}
