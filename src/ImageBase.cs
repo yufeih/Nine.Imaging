cfe2e3c5b0ddc055515fd17a095d38ffeef27745
Yufei Huang <yufeih@live.com>
2015-03-24 19:37:38 +0800

    Remove DefaultDelay

--- src/ImageBase.cs
+++ src/ImageBase.cs
@@ -6,48 +6,24 @@
 // All rights reserved.
 // ===============================================================================
 
-using System;
-
 namespace Nine.Imaging
 {
+    using System;
+
     /// <summary>
     /// Base classes for all Images.
     /// </summary>
     public partial class ImageBase
     {
-        #region Constants
-
-        /// <summary>
-        /// The default animation speed, when the image is animated.
-        /// </summary>
-        public const int DefaultDelayTime = 10;
-
-        #endregion
-
         #region Properties
-
-        private int _delayTime;
+        
         /// <summary>
 		/// If not 0, this field specifies the number of hundredths (1/100) of a second to 
 		/// wait before continuing with the processing of the Data Stream. 
 		/// The clock starts ticking immediately after the graphic is rendered. 
 		/// This field may be used in conjunction with the User Input Flag field. 
 		/// </summary>
-        public int DelayTime
-        {
-            get
-            {
-                int delayTime = _delayTime;
-
-                if (delayTime <= 0)
-                {
-                    delayTime = DefaultDelayTime;
-                }
-
-                return delayTime;
-            }
-            set { _delayTime = value; }
-        }
+        public int? DelayTime { get; set; }
 
         private byte[] _pixels;
         /// <summary>
