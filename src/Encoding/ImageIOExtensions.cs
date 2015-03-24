1b4a51eb08ac43526ce6089cb7fb90b9a2efe9bd
Yufei Huang <yufeih@live.com>
2015-03-24 21:42:18 +0800

    Add quality to SaveAsJpeg method

--- src/Encoding/ImageIOExtensions.cs
+++ src/Encoding/ImageIOExtensions.cs
@@ -7,8 +7,8 @@
     [EditorBrowsable(EditorBrowsableState.Never)]
     public static class ImageIOExtensions
     {
-        public static void SaveAsPng(this Image image, Stream stream) => new PngEncoder().Encode(image, stream);
-        public static void SaveAsJpeg(this Image image, Stream stream) => new JpegEncoder().Encode(image, stream);
-        public static void Save(this Image image, Stream stream, IImageEncoder encoder) => encoder.Encode(image, stream);
+        public static void SaveAsPng(this ImageBase image, Stream stream) => new PngEncoder().Encode(image, stream);
+        public static void SaveAsJpeg(this ImageBase image, Stream stream, int quality = 80) => new JpegEncoder { Quality = quality }.Encode(image, stream);
+        public static void Save(this ImageBase image, Stream stream, IImageEncoder encoder) => encoder.Encode(image, stream);
     }
 }
--- test/EncoderDecoderTest.cs
+++ test/EncoderDecoderTest.cs
@@ -19,7 +19,7 @@
             var stream = File.OpenRead(filename);
             var watch = Stopwatch.StartNew();
             var image = new Image(stream);
-            
+
             var encodedFilename = "Encoded/" + Path.GetFileName(filename);
 
             if (!image.IsAnimated)
@@ -32,13 +32,42 @@
             }
             else
             {
-                foreach (var frame in image.Frames)
+                using (var output = File.OpenWrite($"Encoded/{ Path.GetFileNameWithoutExtension(filename) }.jpg"))
                 {
-                    // TODO: save the frame
+                    image.SaveAsJpeg(output, 40);
+                }
+
+                for (int i = 0; i < image.Frames.Count; i++)
+                {
+                    using (var output = File.OpenWrite($"Encoded/{ i }_{ Path.GetFileNameWithoutExtension(filename) }.png"))
+                    {
+                        image.Frames[i].SaveAsPng(output);
+                    }
                 }
             }
 
             Trace.WriteLine($"{ filename }: { watch.ElapsedMilliseconds}ms");
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(20)]
+        [InlineData(40)]
+        [InlineData(80)]
+        [InlineData(100)]
+        public void jpeg_quality(int quality)
+        {
+            if (!Directory.Exists("Jpeg")) Directory.CreateDirectory("Jpeg");
+            
+            var image = new Image(File.OpenRead("TestImages/Backdrop.jpg"));
+
+            using (var output = File.OpenWrite($"Jpeg/{ quality }.jpg"))
+            {
+                image.SaveAsJpeg(output, quality);
+                output.Flush();
+
+                Trace.WriteLine($"{ quality }: { output.Length }");
+            }
         }
     }
 }
