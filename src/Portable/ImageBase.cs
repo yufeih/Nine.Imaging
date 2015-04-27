c59130d42c450bf5b560da9ddee61af63ee61994
Yufei Huang <yufeih@microsoft.com>
2015-04-27 10:56:27 +0800

    Add MaxWidth/MaxHeight to ImageBase

--- src/Portable/ImageBase.cs
+++ src/Portable/ImageBase.cs
@@ -1,94 +1,37 @@
-﻿// ===============================================================================
-// ImageBase.cs
-// .NET Image Tools
-// ===============================================================================
-// Copyright (c) .NET Image Tools Development Group. 
-// All rights reserved.
-// ===============================================================================
-
-namespace Nine.Imaging
+﻿namespace Nine.Imaging
 {
     using System;
 
-    /// <summary>
-    /// Base classes for all Images.
-    /// </summary>
-    public partial class ImageBase
+    public class ImageBase
     {
-        #region Properties
-
+        /// <summary>
+        /// Returns all pixels of the image in g, b, r, a byte order.
+        /// </summary>
+        public byte[] Pixels => _pixels;
         private byte[] _pixels;
-        /// <summary>
-        /// Returns all pixels of the image as simple byte array.
-        /// </summary>
-        /// <value>All image pixels as byte array.</value>
-        /// <remarks>The returned array has a length of Width * Length * 4 bytes
-        /// and stores the red, the green, the blue and the alpha value for
-        /// each pixel in this order.</remarks>
-        public byte[] Pixels
-        {
-            get { return _pixels; }
-        }
-
+        
+        public int PixelHeight => _pixelHeight;
         private int _pixelHeight;
-        /// <summary>
-        /// Gets the height of this <see cref="Image"/> in pixels.
-        /// </summary>
-        /// <value>The height of this image.</value>
-        /// <remarks>The height will be initialized by the constructor
-        /// or when the data will be pixel data will set.</remarks>
-        public int PixelHeight
-        {
-            get { return _pixelHeight; }
-        }
 
+        public int PixelWidth => _pixelWidth;
         private int _pixelWidth;
-        /// <summary>
-        /// Gets the width of this <see cref="Image"/> in pixels.
-        /// </summary>
-        /// <value>The width of this image.</value>
-        /// <remarks>The width will be initialized by the constructor
-        /// or when the data will be pixel data will set.</remarks>
-        public int PixelWidth
-        {
-            get { return _pixelWidth; }
-        }
 
-        /// <summary>
-        /// Gets the ratio between the width and the height of this <see cref="ImageBase"/> instance.
-        /// </summary>
-        /// <value>The ratio between the width and the height.</value>
-        public double PixelRatio
-        {
-            get { return (double)PixelWidth / PixelHeight; }
-        }
+        public double PixelRatio => (double)PixelWidth / PixelHeight;
 
-        /// <summary>
-        /// Gets or sets the color of a pixel at the specified position.
-        /// </summary>
-        /// <param name="x">The x-coordinate of the pixel. Must be greater
-        /// than zero and smaller than the width of the pixel.</param>
-        /// <param name="y">The y-coordinate of the pixel. Must be greater
-        /// than zero and smaller than the width of the pixel.</param>
-        /// <value>The color of the pixel.</value>
-        /// <exception cref="ArgumentException">
-        ///     <para><paramref name="x"/> is smaller than zero or greater than
-        ///     the width of the image.</para>
-        ///     <para>- or -</para>
-        ///     <para><paramref name="y"/> is smaller than zero or greater than
-        ///     the height of the image.</para>
-        /// </exception>
+        public static int MaxWidth { get; set; } = 5000;
+        public static int MaxHeight { get; set; } = 5000;
+        
         public Color this[int x, int y]
         {
             get
             {
-                int start = (y * PixelWidth + x) * 4;
+                int start = (y * _pixelWidth + x) * 4;
 
                 return new Color(_pixels[start + 3], _pixels[start + 2], _pixels[start + 1], _pixels[start + 0]);
             }
             set
             {
-                int start = (y * PixelWidth + x) * 4;
+                int start = (y * _pixelWidth + x) * 4;
 
                 _pixels[start + 0] = value.B;
                 _pixels[start + 1] = value.G;
@@ -96,108 +39,41 @@ namespace Nine.Imaging
                 _pixels[start + 3] = value.A;
             }
         }
+        
+        public Rectangle Bounds => new Rectangle(0, 0, _pixelWidth, _pixelHeight);
 
-        /// <summary>
-        /// Calculates a new rectangle which represents 
-        /// the dimensions of this image.
-        /// </summary>
-        /// <value>The <see cref="Rectangle"/> object, which
-        /// represents the image dimension.</value>
-        public Rectangle Bounds
-        {
-            get
-            {
-                return new Rectangle(0, 0, PixelWidth, PixelHeight);
-            }
-        }
-
-        #endregion
-
-        #region Constructors
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ImageBase"/> class
-        /// with the height and the width of the image.
-        /// </summary>
-        /// <param name="width">The width of the image in pixels.</param>
-        /// <param name="height">The height of the image in pixels.</param>
-        /// <exception cref="ArgumentException">
-        ///     <para><paramref name="width"/> is equals or less than zero.</para>
-        ///     <para>- or -</para>
-        ///     <para><paramref name="height"/> is equals or less than zero.</para>
-        /// </exception>
+        public ImageBase() { }
         public ImageBase(int width, int height)
         {
-            if (width < 0) throw new ArgumentException("Width must be greater or equals than zero.");
-            if (height < 0) throw new ArgumentException("Height must be greater or equals than zero.");
+            if (width < 0 || width > MaxWidth) throw new ArgumentOutOfRangeException($"Width must be between 0 and { MaxWidth }");
+            if (height < 0 || height > MaxHeight) throw new ArgumentOutOfRangeException($"Height must be between 0 and { MaxHeight }");
 
             _pixelWidth = width;
             _pixelHeight = height;
 
-            _pixels = new byte[PixelWidth * PixelHeight * 4];
+            _pixels = new byte[_pixelWidth * _pixelHeight * 4];
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ImageBase"/> class
-        /// by making a copy from another image.
-        /// </summary>
-        /// <param name="other">The other, where the clone should be made from.</param>
-        /// <exception cref="ArgumentNullException"><paramref name="other"/> is null
-        /// (Nothing in Visual Basic).</exception>
-        /// <exception cref="ArgumentException"><paramref name="other"/> is not loaded.</exception>
         public ImageBase(ImageBase other)
         {
-            if (other == null) throw new ArgumentNullException("Other image cannot be null.");
-
             byte[] pixels = other.Pixels;
 
-            _pixelWidth  = other.PixelWidth;
-            _pixelHeight = other.PixelHeight;
+            _pixelWidth  = other._pixelWidth;
+            _pixelHeight = other._pixelHeight;
             _pixels = new byte[pixels.Length];
 
             Array.Copy(pixels, _pixels, pixels.Length);
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ImageBase"/> class.
-        /// </summary>
-        public ImageBase()
-        {
-        }
-
-        #endregion
-
-        #region Methods
-
-        /// <summary>
-        /// Sets the pixel array of the image.
-        /// </summary>
-        /// <param name="width">The new width of the image.
-        /// Must be greater than zero.</param>
-        /// <param name="height">The new height of the image.
-        /// Must be greater than zero.</param>
-        /// <param name="pixels">The array with colors. Must be a multiple
-        /// of four, width and height.</param>
-        /// <exception cref="ArgumentException">
-        /// 	<para><paramref name="width"/> is smaller than zero.</para>
-        /// 	<para>- or -</para>
-        /// 	<para><paramref name="height"/> is smaller than zero.</para>
-        /// 	<para>- or -</para>
-        /// 	<para><paramref name="pixels"/> is not a multiple of four, 
-        /// 	width and height.</para>
-        /// </exception>
-        /// <exception cref="ArgumentNullException"><paramref name="pixels"/> is null.</exception>
         public void SetPixels(int width, int height, byte[] pixels)
         {
-            if (width < 0) throw new ArgumentException("Width must be greater or equals than zero.");
-            if (height < 0) throw new ArgumentException("Height must be greater or equals than zero.");
+            if (width < 0 || width > MaxWidth) throw new ArgumentOutOfRangeException($"Width must be between 0 and { MaxWidth }");
+            if (height < 0 || height > MaxHeight) throw new ArgumentOutOfRangeException($"Height must be between 0 and { MaxHeight }");
             if (pixels.Length != width * height * 4) throw new ArgumentException("Pixel array must have the length of width * height * 4.");
 
             _pixelWidth  = width;
             _pixelHeight = height;
             _pixels = pixels;
         }
-
-        #endregion
     }
 }
