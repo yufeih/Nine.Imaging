a355f9a25c35ba3645c720338f642d72da42398f
Yufei Huang <yufeih@live.com>
2016-06-24 14:16:51 +0800

    Remove BarCode and Point

--- src/Http/ExtensionMethodInvoker.cs
+++ src/Http/ExtensionMethodInvoker.cs
@@ -122,7 +122,6 @@
             if (type == typeof(double)) return double.Parse(value);
             if (type == typeof(Color)) return Color.Parse(value);
             if (type == typeof(Rectangle)) return Rectangle.Parse(value);
-            if (type == typeof(Point)) return Point.Parse(value);
             if (type.GetTypeInfo().IsEnum) return Enum.Parse(type, value, true);
 
             return null;
